28adf7929d1491067832c9a7e7bdc3f2632b30b8
Karesis <yangyifeng23@mails.ucas.ac.cn>
2025-09-17 22:22:27 +0800

    feat: Add more parser errors kind.

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,5 @@
-use super::Span;
+use crate::Span;
+use crate::tokens::TokenType;
 
 #[derive(Debug, Clone)]
 pub struct LexerError {
@@ -23,9 +24,37 @@ pub enum LexerErrorKind {
 
 #[derive(Debug, Clone)]
 pub struct ParserError {
-    // ... 具体的错误信息 ...
+    pub kind: ParserErrorKind,
     pub span: Span,
 }
+
+#[derive(Debug, Clone)]
+pub enum ParserErrorKind {
+    /// 当遇到的 Token 不是期望的时。这是最常见的语法错误。
+    /// e.g., "Expected a '}' to close the block, but found a 'fun' keyword instead."
+    UnexpectedToken {
+        expected: String, // 对期望内容的描述, e.g., "a type annotation" or "`}`"
+        found: TokenType, // 实际遇到的 Token 类型
+    },
+    /// 当需要一个表达式，但没有找到时。
+    /// e.g., "return ;" -> "Expected an expression after 'return'."
+    ExpectedExpression,
+    
+    /// 当需要一个名称（标识符），但遇到了其他东西时。
+    /// e.g., "fun 123() {}" -> "Expected a function name."
+    ExpectedIdentifier,
+
+    // 不允许顶层语句（即全局变量）
+    NoToplevelStatements,
+    // 我们可以根据需要添加更多...
+}
+
+impl ParserError {
+    pub fn new(kind: ParserErrorKind, span: Span) -> Self {
+        Self { kind, span }
+    }
+}
+
 
 #[derive(Debug, Clone)]
 pub enum CompilerError {
